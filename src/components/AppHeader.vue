bd94f2b6ca94110adb10d6ba9299e70b210ff5cd
Natali Poliakova <99250547+Nataliphoenix@users.noreply.github.com>
2023-11-13 20:58:19 +0200

    header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,36 @@
+<template>
+   <header class="header">
+      <ContainerMain>
+         <Logo />
+      </ContainerMain>
+   </header>
+</template>
+
+<script>
+
+import ContainerMain from './shared/ContainerMain.vue';
+import Logo from './LogoEl.vue'
+
+export default {
+   name: 'AppHeader',
+   components: {
+      ContainerMain,
+      Logo,
+   }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import './src/assets/scss/variables.scss';
+
+.header {
+   position: absolute;
+   top: 0;
+   left: 0;
+   width: 100%;
+
+   padding: 16px 0;
+   background: $secondary-color;
+}
+</style>
---
+++ src/components/LogoEl.vue
@@ -0,0 +1,12 @@
+<template>
+   <img src="../assets/svg/logo.svg" alt="" />
+</template>
+
+<script>
+export default {
+   name: 'LogoEl'
+
+}
+</script>
+
+<style lang="scss" scoped></style>
